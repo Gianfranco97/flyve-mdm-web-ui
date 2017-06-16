c12724c8ff2cc25ccbc8ce2e634d7d9456abaa9e
Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>
2017-06-16 10:14:48 -0400

    Change of Css Framework (Fundation -> winjs/grid)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import Login from './Login'
 import './App.css'
+import 'normalize.css'
+// import '../node_modules/winjs/css/ui-ligth.min.css'
 
 function App() {
   return <Login />
--- src/Login/index.tsx
+++ src/Login/index.tsx
@@ -37,23 +37,24 @@ export default class Login extends React.Component<any, any> {
     render () {
     
         return (
-            <div className="row">
-                <div className="large-4 columns section1">
-                    <img src="img/logo-flyve-login.png" className="img-login"/>
+            <div className="ms-grid">
+                <div className="ms-row">
+                    <div className="m-col-4-12 section1">
+                        <img src="img/logo-flyve-login.png" className="img-login"/>
+                    </div>
+                    <div className="m-col-8-12 section2">
+                        <h2>Mobile Device Management</h2>
+                        <form>
+                            <p>Email</p>
+                            <input type='text' name="email" value={this.state.email} onChange={this.ChangeInput} />
+                            <p>Password</p>
+                            <input type='password' name="password" value={this.state.password} onChange={this.ChangeInput} />
+                        </form>
+                        <span className="credentials" >A solution powered by <img src="img/logo-teclib-blanc-1-2.png" /></span>
+                        <button className="win-button">SIGNUP</button>
+                        <button className="win-button button-form-login" onClick={this.SaveInServer}>LOGIN</button>
+                    </div>
                 </div>
-                <div className="large-8 columns section2">
-                    <h2>Mobile Device Management</h2>
-                    <form>
-                        <p>Email</p>
-                        <input type='text' name="email" value={this.state.email} onChange={this.ChangeInput} />
-                        <p>Password</p>
-                        <input type='password' name="password" value={this.state.password} onChange={this.ChangeInput} />
-                    </form>
-                    <span className="credentials" >A solution powered by <img src="img/logo-teclib-blanc-1-2.png" /></span>
-                    <button className="win-button">SIGNUP</button>
-                    <button className="win-button button-form-login" onClick={this.SaveInServer}>LOGIN</button>
-                </div>
-            
             </div>
         )
     }
